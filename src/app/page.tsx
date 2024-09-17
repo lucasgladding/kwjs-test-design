d04c68fb9e3d929a6dd7debfce9d6599be284fa7
lucasgladding <l.gladding@vehikl.com>
2024-09-16 23:03:04 -0400

    Fixed routing issues.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import Link from "next/link";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <div>
-                <Link href="/ag-grid">Ag Grid</Link>
-            </div>
-        </div>
-    );
+    return <div>Home</div>;
 }
